9ad602c74c4ee0665e1bd294788aa5ae37fad87c
rajaonahsoa <r.lalainafr@gmail.com>
2024-01-30 20:01:12 +0100

    topic, message models - OneToMany relationship

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from base.models import Room
+from base.models import Room, Message, Topic
 
 admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
+
