c70a5d59f1d2b2fe71a2d9a51089411d0629b87a
rajaonahsoa <r.lalainafr@gmail.com>
2024-02-12 20:39:10 +0100

    upload file - create imageField

--- laxDiscord/settings.py
+++ laxDiscord/settings.py
@@ -130,10 +130,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# URL poru les fichiers uploaded
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+# store the static file
+# file uploaded once the form submitted
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- laxDiscord/urls.py
+++ laxDiscord/urls.py
@@ -2,11 +2,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# necéssaire pour les fichiers uploaded
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     
     # on inclut l'url de l'api
     path('api/', include('base.api.urls')),
-
 ]
+
+# set the  url  and get the file from MEDIA_ROOT 
+# connect MEDIA_ROOT to MEDIA_URL
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
